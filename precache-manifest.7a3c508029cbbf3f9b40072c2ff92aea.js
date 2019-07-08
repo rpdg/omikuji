1cb563c18937a22c59a58d4ed32f9f3d73f85e16
sprout2000 <office@nishigami.jp>
2019-07-08 17:22:57 +0900

    Updates

---
+++ precache-manifest.7a3c508029cbbf3f9b40072c2ff92aea.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4d049074d5e345504f7",
+    "url": "bundle.js"
+  },
+  {
+    "revision": "8120bf342074ddb260b49e292643b216",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "c6b7a0ec70a1d623c4cf4b18c021affa",
+    "url": "icons\\icon-144.png"
+  },
+  {
+    "revision": "288e80ffc86674691023bed7fe460e76",
+    "url": "icons\\icon-192.png"
+  },
+  {
+    "revision": "7b552a5ba50a8b7d8294b9182c5aa31f",
+    "url": "icons\\icon-512.png"
+  },
+  {
+    "revision": "b351bd62abcd96e924d9f44a3da169a7",
+    "url": "img/Material-Design-Iconic-Font.ttf"
+  },
+  {
+    "revision": "d2a55d331bdd1a7ea97a8a1fbb3c569c",
+    "url": "img/Material-Design-Iconic-Font.woff"
+  },
+  {
+    "revision": "a4d31128b633bc0b1cc1f18a34fb3851",
+    "url": "img/Material-Design-Iconic-Font.woff2"
+  },
+  {
+    "revision": "ae5c346838fdfc1461cdd50412324cb7",
+    "url": "img/chuukichi.png"
+  },
+  {
+    "revision": "f2df61f499edb5cbb1807ddfa427784b",
+    "url": "img/cover.png"
+  },
+  {
+    "revision": "b7f05f0ad67e1dc770b1c86915e6c9eb",
+    "url": "img/daikichi.png"
+  },
+  {
+    "revision": "89dcf418b91104cf1bc13abb604ea480",
+    "url": "img/daikyou.png"
+  },
+  {
+    "revision": "e8019d507e8cb51d169ab4f94a0cda12",
+    "url": "img/fa-brands-400.eot"
+  },
+  {
+    "revision": "83e6c29fb363b2f0ea6cc18fefff729c",
+    "url": "img/fa-brands-400.svg"
+  },
+  {
+    "revision": "fdf44bc43e8fa2358bbb7d9165d78455",
+    "url": "img/fa-brands-400.ttf"
+  },
+  {
+    "revision": "da408238128b876cbda6424391f1566f",
+    "url": "img/fa-brands-400.woff"
+  },
+  {
+    "revision": "6814d0e8136d34e313623eb7129d538e",
+    "url": "img/fa-brands-400.woff2"
+  },
+  {
+    "revision": "e6c93cb47e716b579264a5fdfbf7e84d",
+    "url": "img/fa-regular-400.eot"
+  },
+  {
+    "revision": "ba2a91dc95e6cfdc4b2a186a7ba83e29",
+    "url": "img/fa-regular-400.svg"
+  },
+  {
+    "revision": "8d220c793e2612bd131ed8522c54669f",
+    "url": "img/fa-regular-400.ttf"
+  },
+  {
+    "revision": "dad90637f797356bbc70d2664832e0b6",
+    "url": "img/fa-regular-400.woff"
+  },
+  {
+    "revision": "8d9ab84bfe87a3f77112a6698cf639fb",
+    "url": "img/fa-regular-400.woff2"
+  },
+  {
+    "revision": "ea363ed422723673917901680be9b37c",
+    "url": "img/fa-solid-900.eot"
+  },
+  {
+    "revision": "de1d242d8acb26ec43c0d071fe78e72d",
+    "url": "img/fa-solid-900.svg"
+  },
+  {
+    "revision": "132e9759d93e4eefd7cdde0d7a322991",
+    "url": "img/fa-solid-900.ttf"
+  },
+  {
+    "revision": "2d0415fa29ea596b7a02c78eddeede20",
+    "url": "img/fa-solid-900.woff"
+  },
+  {
+    "revision": "b75b4bfe0d58faeced5006c785eaae23",
+    "url": "img/fa-solid-900.woff2"
+  },
+  {
+    "revision": "19e65b89cee273a249fba4c09b951b74",
+    "url": "img/ionicons.eot"
+  },
+  {
+    "revision": "aff28a207631f39ee0272d5cdde43ee7",
+    "url": "img/ionicons.svg"
+  },
+  {
+    "revision": "dd4781d1acc57ba4c4808d1b44301201",
+    "url": "img/ionicons.ttf"
+  },
+  {
+    "revision": "2c159d0d05473040b53ec79df8797d32",
+    "url": "img/ionicons.woff"
+  },
+  {
+    "revision": "5c60f5b5f2db8e350ed816ddbe47bcf5",
+    "url": "img/kichi.png"
+  },
+  {
+    "revision": "52d95556a9e6a48fa26b5a1574cda461",
+    "url": "img/kyou.png"
+  },
+  {
+    "revision": "da1437d6d5e60cd9628bfc9c51495f97",
+    "url": "img/omikuji.png"
+  },
+  {
+    "revision": "72a332f05fe254ca981ec748d4874fd4",
+    "url": "img/suekichi.png"
+  },
+  {
+    "revision": "70565be90791f7dbb404a39a8e681fc5",
+    "url": "img/syoukichi.png"
+  },
+  {
+    "revision": "93fc9a4d44214f72c6bf36cb1a9b9fec",
+    "url": "index.html"
+  },
+  {
+    "revision": "6e0685d24ff1f58a41752b8da03fb993",
+    "url": "ios\\apple-touch-icon.png"
+  },
+  {
+    "revision": "e4d049074d5e345504f7",
+    "url": "main.css"
+  },
+  {
+    "revision": "306258a6cddb900f1f067912a7864f2e",
+    "url": "manifest.json"
+  }
+]);
